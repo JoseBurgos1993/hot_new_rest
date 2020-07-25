f55cc767df4eb6ab1126789b4dbfb29c51cd6ba9
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-07-25 12:04:26 -0400

    Added tables and waitlist variables, and paths to display json objects for them on server.js

--- server.js
+++ server.js
@@ -9,9 +9,24 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// reservation variables
+let tables = [];
+let waitlist = [];
+
 //Routes
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
+});
+
+
+//display reservations json
+app.get("api/tables", function (req, res) {
+    return res.json(tables)
+});
+
+//display waitlist json
+app.get("api/waitlist", function (req, res) {
+    return res.json(waitlist)
 });
 
 
